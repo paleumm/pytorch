908e2a564d26d147dbd74af42dec5e4692988a51
paleumm <zdotqdot@gmail.com>
2022-06-09 01:35:57 +0700

    quickstart tutorial

---
+++ quickstart/load_checkpoint.py
@@ -0,0 +1,26 @@
+from quickstart.tutorial import NeuralNetwork
+from quickstart.tutorial import test_data
+import torch
+
+model = NeuralNetwork()
+model.load_state_dict(torch.load("model.pth"))
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f"Predicted: {predicted}, Actual: {actual}")
